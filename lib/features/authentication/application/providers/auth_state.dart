fcec1e9ceead5f5e27728ecca76366ccff70e987
Jondry Suitela <jondrysuitela4@gmail.com>
2026-06-30 22:15:25 +0000

    feat(auth): load current user on bootstrap

--- lib/features/authentication/application/providers/auth_state.dart
+++ lib/features/authentication/application/providers/auth_state.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-import '../../domain/entities/app_user.dart';
 import '../../data/repositories/auth_repository.dart';
+import '../../domain/entities/app_user.dart';
 
 final authRepositoryProvider = Provider<AuthRepository>((ref) {
   return SupabaseAuthRepository(Supabase.instance.client);
@@ -10,15 +10,18 @@ final authRepositoryProvider = Provider<AuthRepository>((ref) {
 
 final authStateProvider = StateNotifierProvider<AuthNotifier, AppUser?>((ref) {
   final repository = ref.watch(authRepositoryProvider);
-  return AuthNotifier(repository);
+  return AuthNotifier(repository)..restoreSession();
 });
 
 class AuthNotifier extends StateNotifier<AppUser?> {
-  AuthNotifier(this._repository) : super(null) {
-    _repository.onAuthStateChanged((user) => state = user);
-  }
+  AuthNotifier(this._repository) : super(null);
 
   final AuthRepository _repository;
+
+  Future<void> restoreSession() async {
+    state = await _repository.getCurrentUser();
+    _repository.onAuthStateChanged((user) => state = user);
+  }
 
   Future<void> signIn({required String email, required String password}) async {
     state = await _repository.signIn(email: email, password: password);
